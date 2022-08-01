591b5701a2748ada216d73492dc174ecc96960b0
Chester Cheung <cheung319@gmail.com>
2022-08-01 16:35:45 -0400

    Controller relationships added

--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -2,12 +2,12 @@ class InvestorsController < ApplicationController
 
     get "/investors" do
     investors = Investor.all
-    investors.to_json
+    investors.to_json(include: [:stock_transactions])
     end
 
     get "/investors/:id" do
     investors = Investor.find(params[:id])
-    investors.to_json
+    investors.to_json(include: [:stock_transactions])
     end
 
 
